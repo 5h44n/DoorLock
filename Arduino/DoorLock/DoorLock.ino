fce0734afb5a2ee843480906cd3364f5b95fd913
shaan savarirayan <shaan@u.northwestern.edu>
2018-11-28 15:06:28 -0500

    Basic Blynk functionality

--- Arduino/DoorLock/DoorLock.ino
+++ Arduino/DoorLock/DoorLock.ino
@@ -1,8 +1,21 @@
 #include <WiFi.h>;
+#include <WiFiClient.h>;
+#include <BlynkSimpleEsp32.h>;
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "8*********************************d";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "Savarirayan Home";
+char pass[] = "*******";
+
 
 void setup() {
-  // put your setup code here, to run once:
-
+  // Debug console
+  Serial.begin(9600);
+  Blynk.begin(auth, ssid, pass);
 }
 
 void loop() {
